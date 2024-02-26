f409f67740019cbbdae3b3540b549aa2962b92e0
“Nidhi <ps4151722@gmail.com>
2024-02-26 15:50:00 +0530

    Timer working

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -17,21 +17,27 @@ class ViewController: UIViewController {
    
     let eggTimes = ["Soft" : 3 , "Medium": 4 , "Hard": 7]
     
-    var secondsRemaining = 60
+    var totalTime = 0
+    var secondsPassed = 0
     
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
+      
         timer.invalidate()
         let hardness = sender.currentTitle!
-        secondsRemaining = eggTimes[hardness]!
+        totalTime = eggTimes[hardness]!
+        progressBar.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer(){
-        if secondsRemaining > 0{
-            secondsRemaining -= 1
-            
-        }else{
+        if secondsPassed < totalTime{
+            secondsPassed += 1
+
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+                    }else{
             timer.invalidate()
             titleLabel.text = "Done!"
         }
